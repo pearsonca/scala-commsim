65df38dd4bfa87ea9bd9498f98d33dd2b9e89f3c
Carl Pearson <cap10@ufl.edu>
2013-04-26 15:37:07 -0400

    switching to factory approach
    expected use: make a factory for each Person type (i.e., communication
    probability in this case) then fetch people

--- src/edu/cap10/person/Person.scala
+++ src/edu/cap10/person/Person.scala
@@ -4,29 +4,42 @@ import scala.actors._
 import scala.actors.Actor._
 import scala.collection.mutable.{Set => MSet};
 import scala.collection.mutable._;
+import scala.util.Random.shuffle
 
 import edu.cap10.channels.Path
 import edu.cap10.message._
-import edu.cap10.sim._
+//import edu.cap10.sim._
 import edu.cap10.clock._
 import edu.cap10.utils._
+import edu.cap10.distributions._
 
-class Person(val id: Int) extends PersonLike {
-  def messages() = {
-    Map[CommunityType,Buffer[(Int,Vocabulary)]]()
+
+class BackgroundFactory(pComm:Double, pBad:Double) {
+  require(pComm >= 0 && pComm <= 1,"pComm is not a probability.")
+  require(pBad >= 0 && pBad <= 1,"pBad is not a probability.")
+  
+  val binCache = BinomialCache(pComm)
+  val src : Stream[PersonLike] = {
+    def loop(i:Int) : Stream[PersonLike] = {
+      new PersonLike {
+        val id = i
+        def messages() = {
+          val res = { for (community <- contacts.keys; 
+        		  commContacts = contacts(community);
+        		  count = binCache(commContacts.length).next;
+        		  if count != 0) yield 
+            community -> shuffle(commContacts).take(count)
+          }.toMap
+          Map[CommunityType,Buffer[(Int,Vocabulary)]]()
+        }
+      } #:: loop(i+1)
+    }
+    loop(0)
   }
 }
 
-object Person {
-	def apply(id:Int) = new Person(id)
-}
-
-class Plotter(id : Int) extends Person(id) {
-	override def act = {
-	  loop {
-	    
-	  }
-	}
+class Plotter(val id : Int) extends PersonLike {
+	def messages = Map[CommunityType,Buffer[(Int,Vocabulary)]]()
 }
 
 //class TestPerson extends Actor {
