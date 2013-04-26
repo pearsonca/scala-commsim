15a736df5eb11f30c8699e32dd0043de00eaf3a9
Carl Pearson <cap10@ufl.edu>
2013-04-26 15:35:39 -0400

    meh test changes

--- src/edu/cap10/distributions/DistroTest.scala
+++ src/edu/cap10/distributions/DistroTest.scala
@@ -6,6 +6,6 @@ object DistroTest {
 		val cache = new BinomialCache(0.3)
 	//	for (max <- 5 to 10; intgen = cache(max)) println(intgen.list(10))
 	//	for (max <- 1 to 10; intgen = cache(max)) println(intgen.list(10))
-		println(cache(6).list(100))
+		println(cache(40).list(10))
 	}
 }
