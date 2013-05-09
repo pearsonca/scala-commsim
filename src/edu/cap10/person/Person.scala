05977a165f4e9ce59a837193cc97dfbd3ccec574
Carl Pearson <cap10@ufl.edu>
2013-05-09 15:53:22 -0400

    plotter factory - plus plot cluster initial

--- src/edu/cap10/person/Person.scala
+++ src/edu/cap10/person/Person.scala
@@ -29,6 +29,10 @@ class BackgroundFactory(pComm:Double, pBad:Double, startId : Int = 0) {
   }
 }
 
+object BackgroundFactory {
+  def apply(pComm:Double, pBad:Double) = new BackgroundFactory(pComm, pBad)
+}
+
 class Person(val id:Int, binCache: BinomialCache, pBad:Double) extends PersonLike {
   override val contacts = Map(
 			Religion -> Buffer[PersonLike](),
@@ -49,10 +53,6 @@ class Person(val id:Int, binCache: BinomialCache, pBad:Double) extends PersonLik
 
 }
 
-object BackgroundFactory {
-  def apply(pComm:Double, pBad:Double) = new BackgroundFactory(pComm, pBad)
-}
-
 class Hub(pBadSubs:Double, pBadNorms:Double, pComm:Double, id:Int) extends Person(id, BinomialCache(pComm), pBadNorms) {
   val clusters = Buffer[Buffer[Plotter]]()
   val badCache = BinomialCache(pBadSubs)
@@ -71,26 +71,60 @@ object Hub {
   def apply(pBadSubs:Double, pBadNorms:Double, pComm:Double, id:Int) = new Hub(pBadSubs, pBadNorms, pComm, id)
 }
 
+class PlotterFactory(pInner:Double, pOuter:Double, startId : Int = 0) {
+  require(pInner >= 0 && pInner <= 1,"pInner is not a probability.")
+  require(pOuter >= 0 && pOuter <= 1,"pOuter is not a probability.")
+  
+  val src : Stream[PersonLike] = {
+    def loop(i:Int) : Stream[PersonLike] = {
+      new Plotter(i, pInner, pOuter) #:: loop(i+1)
+    }
+    loop(startId)
+  }
+}
+
+object PlotterFactory {
+  def apply(pInner:Double, pOuter:Double, startId:Int) = new PlotterFactory(pInner, pOuter, startId)
+}
+
+class PlotCluster(val id : Int, val pInner: Double, val pOuter : Double) extends PersonLike {
+  override val contacts = Map( Plot -> Buffer[PersonLike]() )
+  val members = Buffer[Plotter]()
+  var receivedBad = false
+  override def update(msg:Message) {
+	receivedBad |= msg.content == Bad
+	members.random ! msg
+  }
+  override def messages = if (receivedBad) {
+	receivedBad = false
+	val buffer = Buffer[(PersonLike,Vocabulary)]()
+	  
+//	  if (DoubleSrc.next < pInner) { // maybe send a member of inner circle bad message
+//	    buffer += { (cluster(IntRangeSrcCache(cluster.length).next), messenger(Family,Bad)) } // TODO what community type should the message be?
+//	  }
+//	  if (DoubleSrc.next < pInner) { // maybe send another hub a bad message
+//	    otherClusters(IntRangeSrcCache(otherClusters.length).next).listener ! messenger(Family,Bad)
+//	  }
+	  
+	if (!buffer.isEmpty) {
+	   Map( Plot -> buffer )
+	} else Map()
+  } else Map() // do nothing
+}
+
 /**
  * This class provides a convenient wrapper to sub-ordinate clusters.  The Hub
  * or other clusters can send to a cluster, and it takes care of the send-to-random
  * member part.
  */
-class Plotter(val id : Int, val pInner: Double, val pOuter : Double) extends PersonLike {
-	// funky type inference here - 
-	override val contacts = Map( Plot -> Buffer[PersonLike]() )
+class Plotter(val id : Int, val pInner: Double, val pOuter : Double) extends PersonLike { 
+	override val contacts = Map[Community.Value,Buffer[PersonLike]]()
 	
 	val otherClusters = Buffer[Seq[Plotter]]()
+	var tasked : Option[PersonLike] = None
 	
-	// TODO handle setting receivedBad on update(msg)
-	var receivedBad = false
-	override def update(msg:Message) {
-	  receivedBad |= msg.content == Bad
-	  super.update(msg)
-	}
-	
-	override def messages = if (receivedBad) {
-	  receivedBad = false
+	override def messages = if (tasked.isDefined) {
+	  tasked = None
 	  val buffer = Buffer[(PersonLike,Vocabulary)]()
 	  
 //	  if (DoubleSrc.next < pInner) { // maybe send a member of inner circle bad message
