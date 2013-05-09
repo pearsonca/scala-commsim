a9edf704682a7600480ff04b19f5beeb7ab273a5
Carl Pearson <cap10@ufl.edu>
2013-05-09 07:58:37 -0400

    more R script fiddling

--- rscripts/test-output.R
+++ rscripts/test-output.R
@@ -1,4 +1,8 @@
 library(igraph)
-edgeinfo <- t(read.table("../commsim-test.txt")[,-3])
-g <- graph(edgeinfo+1)
-plot(g, edge.arrow.size=0.2)
+edgeinfo <- t(read.table("../commsim-test.txt"))
+g <- graph(as.numeric(edgeinfo[-3,])+1)
+png(file="example_background.png",width=1000,height=1000)
+par(mar=c(0,0,0,0)+0.1)
+plot(g, edge.arrow.size=0.5,
+     vertex.label=NA, vertex.size=2, vertex.frame.color=NA)
+dev.off()
