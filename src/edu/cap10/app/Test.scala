a5a48458c056ef948ed9cc093465b22488e509b0
Carl A. B. Pearson <cap10@ufl.edu>
2013-05-12 16:02:47 -0400

    updated testing

--- src/edu/cap10/app/Test.scala
+++ src/edu/cap10/app/Test.scala
@@ -4,6 +4,11 @@ import edu.cap10.person._
 import edu.cap10.graph._
 import edu.cap10.distributions._
 
+import edu.cap10.person.Vocabulary._ // Good, Bad
+import edu.cap10.person.Community._ // 
+
+import scala.collection.mutable.Buffer
+
 import java.io._
 
 object Test {
@@ -22,14 +27,20 @@ object Test {
 	   
 	   val (clusterCount,clusterSize) = (3,5)
 
-	   val terrorists = PlotClusters(popSize+1, pComm, pBadBack, clusterSize).take(clusterCount)
+	   val terrorists = PlotClusters(-(popSize+1), pComm, pBadBack, clusterSize).take(clusterCount)
 
 	   H.contacts(Community.Plot) ++= Clique(Community.Plot)(terrorists)
-	   val output = cliquer.apply(triads) ++ terrorists :+ H
+	   val output = (cliquer.apply(triads) :+ H) ++ terrorists 
 //	   val (pwEL, pwVI) = (new PrintWriter("./commsim-test.txt"), new PrintWriter("./commsim-test-vertex-info.txt"))
 //	   iGraphELWriter.write(pwEL, output).close 
 //	   iGraphVIWriter.write(pwVI, output).close
-	   output foreach( _.start )
-	   output foreach( a => a ! "TEST")
+//	   output.head.start
+//	   output.head ! "TEST"
+//	   output.head ! "DONE"
+	   val test = SimulationCommand(SimulationEvent.TEST, 0)
+	   val done = SimulationCommand(SimulationEvent.DONE, 0)
+	   output foreach( _ start )
+	   output foreach( _ ! test)
+	   output foreach( _ ! done)
 	}
 }
