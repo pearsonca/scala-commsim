47c860533be3c51742e34388166797b0266537f6
Carl A. B. Pearson <cap10@ufl.edu>
2013-05-13 11:42:30 -0400

    revs to do multiple runs

--- src/edu/cap10/app/Test.scala
+++ src/edu/cap10/app/Test.scala
@@ -12,41 +12,75 @@ import scala.collection.mutable.Buffer
 import java.io._
 
 object Test {
-	def main(args: Array[String]) {
-	   val popSize = 3
+	def main(args: Array[String]) = {
+	  val iterlim = 100
+	  val simlim = 1000
+	  val popSize = 1000
 	   val cliqueSize = 3
-	   val (pBadBack, pBadFore, pBadNormDiscount, pComm) = (0.1, 0.1, 0.5, 0.2)
-	   val factory = BackgroundFactory(pComm, pBadBack)
-	   val people = factory.src.take(popSize)
+	   val (pBadBack, pBadFore, pBadNormDiscount, pComm) = (0.1, 1.0, 0.5, 0.2)
 	   val cliquer = CliqueUp(cliqueSize,Community.Family)
-	   val H = Hub(pBadBack,pBadFore*pBadNormDiscount,pComm, popSize)
 	   val hMeanK = 30
 	   val hConP = (hMeanK * cliqueSize).toDouble / popSize
+	   val (clusterCount,clusterSize) = (3,5)
+	   val test = SimulationCommand(SimulationEvent.TEST, 0)
+	   val done = SimulationCommand(SimulationEvent.DONE, 0)
+	for (iteration <- 0 until iterlim) {
+	  println("starting iteration "+iteration)
+	  val start = System.currentTimeMillis()
+		Logger.start(iteration)
+	   
+	   val factory = BackgroundFactory(pComm, pBadBack)
+	   val people = factory.src.take(popSize)
+	   
+	   val H = Hub(pBadBack,pBadFore*pBadNormDiscount,pComm, popSize)
+	   
 	   val triads = CliqueAll.grouped(people.iterator,popSize, cliqueSize, Community.Family)
 	   for (c <- triads if DoubleSrc.next < hConP) c.random.join(H, Community.Family)
 	   
-	   val (clusterCount,clusterSize) = (3,5)
+	   
 
-	   val terrorists = PlotClusters(-(popSize+1), pComm, pBadBack, clusterSize).take(clusterCount)
+	   val terrorists = PlotClusters(-(popSize+1), pComm, pBadFore, clusterSize).take(clusterCount)
 
 	   H.clusters ++= Clique(Community.Plot)(terrorists)
 	   val output = (cliquer.apply(triads) :+ H) ++ terrorists 
 //	   val (pwEL, pwVI) = (new PrintWriter("./commsim-test.txt"), new PrintWriter("./commsim-test-vertex-info.txt"))
 //	   iGraphELWriter.write(pwEL, output).close 
 //	   iGraphVIWriter.write(pwVI, output).close
-//	   output.head.start
-//	   output.head ! "TEST"
-//	   output.head ! "DONE"
-	   val test = SimulationCommand(SimulationEvent.TEST, 0)
-	   val done = SimulationCommand(SimulationEvent.DONE, 0)
+
 	   output foreach( _ start )
-	   //output foreach( _ ! test)
-	   val simlim = 1
+	   
 	   for (t <- 1 to simlim) {
-		   val results = output map( _ !! SimulationCommand(SimulationEvent.NEXT, 1))
-		   for (res <- results) println(res())
+		   if ( !output.map( _ !! SimulationCommand(SimulationEvent.UPDATE, t)).foldLeft(true)((res,f)=> res && (f() == "ACK")) ) println("failed UPDATE")
+		   if ( !output.map( _ !! SimulationCommand(SimulationEvent.NEXT, t)).foldLeft(true)((res,f)=> res && (f() == "ACK")) ) println("failed NEXT")
 	   }
-	   println("here")
 	   output foreach( _ ! done)
+	   Logger.close
+	   println("completed iteration "+iteration)
+	   println("run time: "+ (System.currentTimeMillis()-start))
 	}
+	}
+}
+
+object Logger {
+  val filter = new FilenameFilter() {
+    def accept(f:File, name:String) = name.contains("current")
+  }
+  var pws : Seq[PrintWriter] = Seq()
+  def start(step:Int=0) = {
+    val others = (new File("./")).listFiles(filter)
+    others.foreach( (file) => file.renameTo(new File(file.getPath.replace("current",step.toString) )) ) 
+    pws = Seq("./test-hub-current.txt","./test-plot-current.txt","./test-cluster-current.txt","./test-back-current.txt") map ( s => new PrintWriter(s) )
+  }
+  def println(src:PersonLike,msg:String) = {
+    src match {
+      case h:Hub => pws(0).println(msg)
+      case pl:Plotter => pws(1).println(msg)
+      case cl:PlotCluster => pws(2).println(msg)
+      case b:Person => pws(3).println(msg)
+    }
+  }
+  def close = {
+    pws foreach { pw => pw.flush; pw.close; }
+  }
+  
 }
