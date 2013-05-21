aed25171d71d484afac8ba82949ed744e9c3a58d
Carl Pearson <cap10@ufl.edu>
2013-05-21 08:55:38 -0400

    add a filter to random
    basically does a random redraw for cases where it doesn't make sense to
    filter the whole Iterable - e.g., one item forbidden

--- src/edu/cap10/distributions/package.scala
+++ src/edu/cap10/distributions/package.scala
@@ -3,6 +3,11 @@ package edu.cap10
 package object distributions {
 	implicit def seqToEnhanced[T](from:Seq[T]) = new RandomDrawSeq[T](from)
 	class RandomDrawSeq[T](src:Seq[T]) {
-	  def random : T = src(IntRangeSrcCache(src.size).next)
+	  def random() : T = src(IntRangeSrcCache(src.size).next)
+	  def random( filter:(T)=>Boolean ) : T = {
+	    var res = random()
+	    while (!filter(res)) res = random()
+	    res
+	  }
 	}
 }
