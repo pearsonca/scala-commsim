002c3f4ffae39be5ca74e5d153a4d0c734b21149
Carl Pearson <cap10@ufl.edu>
2013-08-15 10:25:26 -0400

    seems like ugly overhead
    maybe smarter way to do implicit conversion from Iterable[Repr] to
    Repr*?

--- src/edu/cap10/graph/Vertex.scala
+++ src/edu/cap10/graph/Vertex.scala
@@ -1,6 +1,5 @@
 package edu.cap10.graph
 
-//import scala.collection.mutable.Buffer
 import scala.collection.mutable.SortedSet
 import scala.collection.TraversableLike
 import scala.collection.immutable.Stream.continually;
@@ -20,13 +19,19 @@ object Vertex {
   (trav:TraversableLike[U,Trav]) = trav map { v => subclassToRepr(v) }
 }
 
-trait Vertex[EdgeType, Repr <: Vertex[EdgeType, Repr]] extends Ordering[Repr] {
-  type Self = Repr
-  def compare(x:Repr, y:Repr) = ord.compare(x, y)
-  implicit val ord = Ordering.by( (v:Repr) => v.id )
+trait Vertex[EdgeType, Repr <: Vertex[EdgeType, Repr]] extends Ordered[Repr] with Function1[EdgeType,SortedSet[Repr]] {
+  
+  def compare(that:Repr) = id.compare(that.id)
+  
   def id : Long
   def self = this.asInstanceOf[Repr]
   
+  override val hashCode = id.hashCode
+  override def toString = id.toString
+  override def equals(other:Any) = other match {
+    case o:Vertex[EdgeType,Repr] => o.id == id
+    case _ => false
+  }
 //  def <~>(implicit edge : EdgeType) : ((Repr,Repr)) => Unit = Vertex.<~>[EdgeType,Repr].tupled
   
   def apply(e: EdgeType) = edges(e)
@@ -35,13 +40,34 @@ trait Vertex[EdgeType, Repr <: Vertex[EdgeType, Repr]] extends Ordering[Repr] {
   
   protected def edgeCollSrc = continually(SortedSet[Repr]())
   
-  def ~>[R <: Repr](other:Repr*)
-  (implicit e: EdgeType) = {
-    this(e) ++= other; self
+  def ~>[R <: Repr](other:Repr)
+  (implicit e: EdgeType) : Repr = {
+    this(e) += other
+    self
   }
-  def <~(other:Repr*)
-  (implicit e:EdgeType)  = {
-    other foreach { _ ~> self }; self
+  def ~>[R <: Repr](other1:Repr, other2:Repr, others:Repr*)
+  (implicit e: EdgeType) : Repr = {
+    this(e)+=(other1,other2,others:_*)
+    self
+  }
+  def ~>[R <: Repr](others:Iterable[Repr])
+  (implicit e: EdgeType) : Repr = {
+    this(e) ++= others; self
+  }
+  
+  
+  def <~(other:Repr)
+  (implicit e:EdgeType) : Repr = {
+    other ~> self; self
+  }
+  def <~(other1:Repr, other2:Repr, others:Repr*)
+  (implicit e:EdgeType) : Repr = {
+    other1 ~> self; other2 ~> self
+    <~(others)
+  }
+  def <~(others:Iterable[Repr])
+  (implicit e:EdgeType) : Repr = {
+    others foreach { _ ~> self }; self
   }
 
   def ~/>[R <: Repr](other:Repr*)
@@ -53,14 +79,20 @@ trait Vertex[EdgeType, Repr <: Vertex[EdgeType, Repr]] extends Ordering[Repr] {
     other foreach { _ ~/> self }; self
   }
   
+  //n.b.: this(e) yields a Set, and Sets are a Element => Boolean, specifically the ``contains'' function
   def ?~>(other:Repr)
-  (implicit e: EdgeType) = {
-    this(e) contains other
-  }
+  (implicit e: EdgeType) : Boolean =
+    this(e)(other)
+  def ?~>(other1:Repr, other2:Repr, others:Repr*)
+  (implicit e: EdgeType) : Boolean =
+    (self ?~> other1) && (self ?~> other2) && (self ?~> others)
+  def ?~>(others:Iterable[Repr])
+  (implicit e: EdgeType) : Boolean =
+    others forall { this(e) }
+  
   def <~?(other:Repr)
-  (implicit e: EdgeType) = {
-    other ?~> self
-  }
+  (implicit e: EdgeType) = other ?~> self
+  // TODO other versions
 
   def !~>(other:Repr*)
   (implicit e: EdgeType)  = {
@@ -81,7 +113,10 @@ trait Vertex[EdgeType, Repr <: Vertex[EdgeType, Repr]] extends Ordering[Repr] {
     self
   }
   
-  def <~>(other:Repr*)(implicit e: EdgeType) = ~> (other:_*) <~ (other:_*)
+  def <~>(other:Repr)(implicit e: EdgeType) : Repr = ~>(other)<~(other)
+  def <~>(other1:Repr, other2:Repr, others:Repr*)(implicit e: EdgeType) : Repr = <~>(other1)<~>(other2)<~>(others)
+  def <~>(others:Iterable[Repr])(implicit e: EdgeType) = ~>(others)<~(others)
+  
   def </~/>(other:Repr*)(implicit e: EdgeType) = ~/> (other:_*) </~ (other:_*)
   
   def <~!~>(other:Repr*)(implicit e: EdgeType) = !~> (other:_*) <~! (other:_*)
