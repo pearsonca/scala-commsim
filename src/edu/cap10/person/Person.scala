fdd84090dcac39a17763ded706fcbdf533e51d5b
Carl Pearson <cap10@ufl.edu>
2013-08-15 10:26:31 -0400

    make use of case class to elim helper object

--- src/edu/cap10/person/Person.scala
+++ src/edu/cap10/person/Person.scala
@@ -8,18 +8,13 @@ import edu.cap10.distributions._
 import Community.{Value => CValue, _}
 import Vocabulary.{Value => VValue, _}
 
-class BackgroundFactory(pComm:Double, pBad:Double, startId : Int = 0) {
+case class BackgroundFactory(pComm:Double, pBad:Double, startId : Int = 0) {
   require(pComm >= 0 && pComm <= 1,"pComm is not a probability.")
   require(pBad >= 0 && pBad <= 1,"pBad is not a probability.")
   
   val binCache = BinomialCache(pComm) // set up a binomial distribution cache
   private def loop(i:Int) : Stream[PersonLike] = new Person(i, binCache, pBad) #:: loop(i+1)
   val src : Stream[PersonLike] = loop(startId)
-}
-
-object BackgroundFactory {
-  def apply(pComm:Double, pBad:Double, startId:Int = 0) =
-    new BackgroundFactory(pComm, pBad, startId)
 }
 
 class Person(val id:Long, binCache: BinomialCache, pBad:Double) extends PersonLike {
