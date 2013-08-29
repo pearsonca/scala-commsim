f831c55903e29364d076479db69a4aecf32b0312
Carl Pearson <cap10@ufl.edu>
2013-08-29 12:48:49 -0400

    also naming

--- test/edu/cap10/graph/generators/CliqueTest.scala
+++ test/edu/cap10/graph/generators/CliqueTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.FlatSpec
 import collection.mutable.Stack
 
 import edu.cap10.graph.MockVertexFactory
-import edu.cap10.graph.MockEdge.EDGE
+import edu.cap10.graph.MockEdge.MOCKEDGE
 import edu.cap10.graph.MockVertex
 
 import collection.mutable.SortedSet
@@ -12,20 +12,20 @@ import collection.mutable.SortedSet
 class CliqueTest extends FlatSpec {
   
   val refSize = 5
-  implicit val e = EDGE
+  implicit val e = MOCKEDGE
   val generator = Clique(e)
   def src = MockVertexFactory.take(refSize).toSeq
   
   
   "A Clique" should "return a Seq of its inputs" in {
     val vertices = src
-    val res = generator whole vertices
+    val res = generator clique vertices
     assert(vertices.size === res.size)
   }
   
   it should "connect all of its elements" in {
     val vertices = src
-    val res = generator whole vertices
+    val res = generator clique vertices
     res foreach {
       v => {
         assert( v(e).size === (vertices.size - 1) )
