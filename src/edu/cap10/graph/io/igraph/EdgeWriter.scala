c89ada866a72c2bdf5c56d3656dfcd71e1d8e37b
Carl Pearson <cap10@ufl.edu>
2013-09-01 15:17:50 -0400

    extracting a bit from edgewriter
    the plan being to keep the non-generic bits in vertex/edge specific
    domains

--- src/edu/cap10/graph/io/igraph/EdgeWriter.scala
+++ src/edu/cap10/graph/io/igraph/EdgeWriter.scala
@@ -3,11 +3,12 @@ package edu.cap10.graph.io.igraph
 import edu.cap10.graph.Vertex
 import java.io._
 
-object EdgeWriter {
-	def apply[EdgeType, V <: Vertex[EdgeType,V]](vertices:Iterable[V])(implicit writer:PrintWriter) = {
-	  for (src <- vertices; edgeType <- src.edgeTypes; tar <- src(edgeType))
-	    writer.println(src.id + " " + tar.id + " " + edgeType)
-	  writer.flush
-	  writer
-	}
+trait GenericEdgeWriter[EdgeType, V <: Vertex[EdgeType,V]] {
+  def apply(vertices:Iterable[V])(implicit writer:PrintWriter) = {
+    for ( (v1,v2,e) <- transform(vertices) ) writer.println(v1.id + " " + v2.id + " " + e)
+	writer.flush
+	writer
+  }
+  
+  def transform(vertices:Iterable[V]) : Iterable[(V,V,EdgeType)]
 }
