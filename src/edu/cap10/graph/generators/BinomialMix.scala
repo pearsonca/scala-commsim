40ed3e7da2262c19187de29ca88496d20ec39150
Carl Pearson <cap10@ufl.edu>
2013-09-25 06:58:17 -0400

    semantics tweak

--- src/edu/cap10/graph/generators/BinomialMix.scala
+++ src/edu/cap10/graph/generators/BinomialMix.scala
@@ -13,13 +13,13 @@ case class BinomialMix[EdgeType](implicit val e:EdgeType) extends Generator[Edge
   override def apply
   [V <: Vertex[EdgeType,V]]
   (data : (Seq[V], Double)) = {
-    val (iter, rate) = data
+    val (vertices, rate) = data
     require(0 <= rate && rate <= 1,"rate parameter is not a probability; provided rate: "+rate.toString)
     if (rate != 0d)
-	    iter.dPairs.filter( _ => DoubleSrc.next < rate ).foreach { 
+	    vertices.dPairs.filter( _ => DoubleSrc.next < rate ).foreach { 
 	      p => p._1 !~> p._2
 	    }
-    iter.toSeq
+    vertices
   }
   
   	override implicit def default
