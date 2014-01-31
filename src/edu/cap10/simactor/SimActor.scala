b996dd995c5aa62ed3d341b074ac60af9beef098
Carl A. B. Pearson <cap10@ufl.edu>
2014-01-30 19:33:53 -0500

    some changes

--- src/edu/cap10/simactor/SimActor.scala
+++ src/edu/cap10/simactor/SimActor.scala
@@ -6,26 +6,34 @@ import akka.actor.Actor
 import akka.actor.ActorRef
 
 object SimActor { // defines common elements for SimActors
-  // MESSAGES ABOUT INTERNAL STATE
-  object Radicalize                           // become a plotter
-  object Recruit                              // work for a plotter
-  case class Collaborate(group:Plotters)      // work with other plotters
-  case class Subordinates(recruits:Plotters)  // recruit subordinates
+  object Radicalize // become a plotter
   
-  object PType extends Enumeration { // plotter behavior probabilites
-    type PType = Value
-    val	Initiate, // prob. of starting plot comms
-        Report,   // prob. of reporting to superiors
-    	Collab,	  // prob. of working w/ peers
-    	Delegate  // prob. of delegating work
-    	  = Value
-  }
-  case class UpdateProb(changes:Map[PType.Value,Probability])
-
   // MESSAGES ABOUT EXTERNAL ACTIVITY
-  object Bad
-  object Good
+  object SimEvents extends Enumeration { 
+    val Bad, Good = Value
+  }
+  import SimEvents.{Value => SimEvent, _}
 
+  object Relationships extends Enumeration {
+    val Supervisor, Peer, Subordinate, All = Value
+  }
+  import Relationships.{Value => Relationship, _}
+    
+  object SocialContexts extends Enumeration {
+    val Familial, Economic, Religious, Covert = Value
+  }
+  import SocialContexts.{Value => SocialContext, _}
+  
+  type RelationshipMap = Map[SocialContext,Map[Relationship,People]]
+  type BehaviorMap = Map[SocialContext,Map[Relationship,Probability]]
+  
+  // CREATE RELATIONSHIPS IN PARTICULAR SOCIAL CONTEXTS                   
+  case class Recruit(sc:SocialContext) // add a Supervisor relationship w/ sender
+  case class Peer(peers:People, sc:SocialContext) // add Peer relationship w/ peers
+  case class Supervise(recruits:People, sc:SocialContext) // recruit subordinates
+
+  case class UpdateBehavior(changes:Map[Relationship,Probability], sc:SocialContext)
+  
   case class PlotterState( // the plotter state description
     superiors:Plotters = empty,
     collaborators:Plotters = empty,
@@ -45,43 +53,50 @@ class SimActor extends Actor with BaseSimActor {
   
   def receive = {
     case Radicalize => become( plotter() )
-    case Recruit => become( plotter(PlotterState(superiors = group(sender))) )
+    case Recruit => become( plotter(state = PlotterState(superiors = group(sender))) )
     case _ => // ignore other messages
   }
   
-  def plotter(ps:PlotterState = PlotterState(), time:Long=0) : Receive = {
-    import ps._
-    def update(newPs:PlotterState) = become( plotter(newPs, time) )
-    def step = become( plotter(ps,time+1) )
-    
-    val base : Receive = {
-       case Collaborate(group) =>
-         update(copy(collaborators = collaborators ++ (group - self)))
+  def basePlot(evolve: (PlotterState)=>Unit, state:PlotterState ) : Receive = {
+    import state.{copy => change, _}
+    {  case Collaborate(group) =>
+         evolve(change(collaborators = collaborators ++ (group - self))) 
+       
        case Subordinates(recruits) =>
          recruits foreach { recruit => recruit ! Recruit }
-         update(copy(subordinates = subordinates ++ recruits))
+         evolve(change(subordinates = subordinates ++ recruits))
+       
        case Recruit =>
-         update(copy(superiors = superiors + sender))
+         evolve(change(superiors = superiors + sender))
+       
        case UpdateProb(changes) =>
-         update(copy(probs = probs ++ changes))
-       case Bad if !heardBad => update(copy(heardBad = superiors(sender) || collaborators(sender) || subordinates(sender)))
+         evolve(change(probs = probs ++ changes))
+    }
+  }
+  
+  def plotting(ps:PlotterState, time:Long) : Receive = {
+    case Time(t) if t == time => // going to send some bad messages, then become a plotter
+      // handle collab, etc as if plotting
+  }
+  
+  def plotter(time:Long=0, state:PlotterState = PlotterState()) : Receive = {
+    import state.{copy => change, _}
+    val evolve = (newPs:PlotterState) => become( plotter(time, newPs) )
+    val b = basePlot(evolve, state)
+    
+    def step = become( plotter(time+1, state) )
+    
+    val base : Receive = b orElse {
+       
+       case Bad if (superiors++collaborators++subordinates) contains sender => become( plotting(state,time) )
+       
        case Time(t) if t == time =>
-         val who : People = if (heardBad || random < probs(PType.Initiate)) { // send some bad messages
-           if (!superiors.isEmpty && random < probs(PType.Report)) {
-             empty
-           } else empty
-           if (!collaborators.isEmpty && random < probs(PType.Report)) {
-             empty
-           } else empty
-           if (!subordinates.isEmpty && random < probs(PType.Report)) {
-             empty
-           } else empty
-         } else empty
-         if (who.isEmpty) {
-           
-         } else {
-           awaiting(who)
+         if (random < probs(PType.Initiate)) {
+           // send some bad messages based on my whole cohort
          }
+         // send some Good messages
+         // let me acker know what's up
+
     }
     simBase(step,time) orElse base
   }
