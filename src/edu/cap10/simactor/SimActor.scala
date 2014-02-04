9b9052bb21169acec98f933cc9566ac54206eb86
Carl A. B. Pearson <cap10@ufl.edu>
2014-02-04 08:52:05 -0500

    updating

--- src/edu/cap10/simactor/SimActor.scala
+++ src/edu/cap10/simactor/SimActor.scala
@@ -28,12 +28,9 @@ object SimActor { // defines common elements for SimActors
   type BehaviorMap = Map[ (SocialContext, Relationship), Map[SimEvent, Probability]]
   
   case class UpdateRelationships(sc:SocialContext, rel:Relationship, add:People = empty, remove:People = empty)
-  case class UpdateBehavior(sc:SocialContext, rel:Relationship, event:SimEvent, p:Probability)
+  case class UpdateBehaviors(sc:SocialContext, rel:Relationship, event:SimEvent, p:Probability)
   
-  // CREATE RELATIONSHIPS IN PARTICULAR SOCIAL CONTEXTS                   
-  case class Recruit(sc:SocialContext) // add a Supervisor relationship w/ sender
-  case class Peer(peers:People, sc:SocialContext) // add Peer relationship w/ peers
-  case class Supervise(recruits:People, sc:SocialContext) // recruit subordinates
+  // CREATE RELATIONSHIPS IN PARTICULAR SOCIAL CONTEXTS
   
   case class PersonState( // the plotter state description
     relationships : RelationshipMap = Map.empty,
@@ -46,55 +43,74 @@ import SimRunner._
 
 class SimActor extends Actor with BaseSimActor {
   import SimActor._
-  import context._
   
-  def receive = {
-    case Radicalize => become( plotter() )
-    case Recruit => become( plotter(state = PlotterState(superiors = group(sender))) )
-    case _ => // ignore other messages
+  def evolve : Receive => Unit = context become _
+  
+  def connections(ps:PersonState = PersonState())(implicit other : Receive) : Receive = {
+    import ps.{copy => change, _}  
+	other orElse {
+	  case UpdateRelationships(sc, rel, add, remove) =>
+	    this evolve connections(change(relationships = relationships + ( (sc,rel) -> (relationships((sc,rel)) ++ add -- remove) )))
+	  case UpdateBehaviors(sc, rel, event, p) =>
+	    this evolve connections(change(behaviors = behaviors + ( (sc,rel) -> (behaviors((sc,rel)) + (event -> p)) )))
+	  case Radicalize =>
+	    this evolve connections(ps)(plotter)
+	}
   }
   
-  def basePlot(evolve: (PlotterState)=>Unit, state:PlotterState ) : Receive = {
-    import state.{copy => change, _}
-    {  case Collaborate(group) =>
-         evolve(change(collaborators = collaborators ++ (group - self))) 
-       
-       case Subordinates(recruits) =>
-         recruits foreach { recruit => recruit ! Recruit }
-         evolve(change(subordinates = subordinates ++ recruits))
-       
-       case Recruit =>
-         evolve(change(superiors = superiors + sender))
-       
-       case UpdateProb(changes) =>
-         evolve(change(probs = probs ++ changes))
-    }
+  import SimActor.SimEvents.{Value => SimEvent, _}
+  def plotter : Receive = {
+    case Bad if =>
+    case Good =>
   }
   
-  def plotting(ps:PlotterState, time:Long) : Receive = {
-    case Time(t) if t == time => // going to send some bad messages, then become a plotter
-      // handle collab, etc as if plotting
-  }
-  
-  def plotter(time:Long=0, state:PlotterState = PlotterState()) : Receive = {
-    import state.{copy => change, _}
-    val evolve = (newPs:PlotterState) => become( plotter(time, newPs) )
-    val b = basePlot(evolve, state)
-    
-    def step = become( plotter(time+1, state) )
-    
-    val base : Receive = b orElse {
-       
-       case Bad if (superiors++collaborators++subordinates) contains sender => become( plotting(state,time) )
-       
-       case Time(t) if t == time =>
-         if (random < probs(PType.Initiate)) {
-           // send some bad messages based on my whole cohort
-         }
-         // send some Good messages
-         // let me acker know what's up
-
-    }
-    simBase(step,time) orElse base
-  }
+//  def receive = {
+//    case Radicalize => become( plotter() )
+//    case Recruit => become( plotter(state = PlotterState(superiors = group(sender))) )
+//    case _ => // ignore other messages
+//  }
+//  
+//  def basePlot(evolve: (PlotterState)=>Unit, state:PlotterState ) : Receive = {
+//    import state.{copy => change, _}
+//    {  case Collaborate(group) =>
+//         evolve(change(collaborators = collaborators ++ (group - self))) 
+//       
+//       case Subordinates(recruits) =>
+//         recruits foreach { recruit => recruit ! Recruit }
+//         evolve(change(subordinates = subordinates ++ recruits))
+//       
+//       case Recruit =>
+//         evolve(change(superiors = superiors + sender))
+//       
+//       case UpdateProb(changes) =>
+//         evolve(change(probs = probs ++ changes))
+//    }
+//  }
+//  
+//  def plotting(ps:PlotterState, time:Long) : Receive = {
+//    case Time(t) if t == time => // going to send some bad messages, then become a plotter
+//      // handle collab, etc as if plotting
+//  }
+//  
+//  def plotter(time:Long=0, state:PlotterState = PlotterState()) : Receive = {
+//    import state.{copy => change, _}
+//    val evolve = (newPs:PlotterState) => become( plotter(time, newPs) )
+//    val b = basePlot(evolve, state)
+//    
+//    def step = become( plotter(time+1, state) )
+//    
+//    val base : Receive = b orElse {
+//       
+//       case Bad if (superiors++collaborators++subordinates) contains sender => become( plotting(state,time) )
+//       
+//       case Time(t) if t == time =>
+//         if (random < probs(PType.Initiate)) {
+//           // send some bad messages based on my whole cohort
+//         }
+//         // send some Good messages
+//         // let me acker know what's up
+//
+//    }
+//    simBase(step,time) orElse base
+//  }
 }
