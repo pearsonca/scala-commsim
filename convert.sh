f5bb5bcc43320fa2b0f0d16c1df8d2a5ad5b5f7a
Carl A. B. Pearson <cap10@ufl.edu>
2014-08-06 08:52:54 -0400

    paper generation fiddling

--- convert.sh
+++ convert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-jekyll build --config=_config.yml,_locconfig.yml
+jekyll build --config=_config.yml,_locconfig.yml,_topdfconfig.yml
 echo "built."
-java -jar mathtoweb.jar -dd . -df proc.html _site/index.html
+java -jar mathtoweb.jar -force -dd . -df proc.html _site/index.html
 jekyll serve --config=_config.yml,_locconfig.yml -B
 wkhtmltopdf http://localhost:4000/proc.html test.pdf
