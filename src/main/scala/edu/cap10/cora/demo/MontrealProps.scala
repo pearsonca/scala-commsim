6bf614a96a16e8c52e4f8942f10acd336e15d419
Carl A. B. Pearson <blank101@gmail.com>
2015-02-24 10:52:04 -0500

    remove non-site content from site branch

--- src/main/scala/edu/cap10/cora/demo/MontrealProps.scala
+++
@@ -1,8 +0,0 @@
-package edu.cap10.cora.demo
-import edu.cap10.util.Probability
-import edu.cap10.util.Probability._
-import scala.language.implicitConversions
-
-case class ReferenceConfig(uniqueLocs:Int, totalDays:Int, dailyVisitProb : Probability, averageVisitDuration:Double, avgLocs:Double)
-
-object MontrealProps extends ReferenceConfig(345, 356*5, 0.5, 1, 4)
