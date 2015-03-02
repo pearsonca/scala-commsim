381dc2183fc7fc8b3dbe42a5d3d7b09b1266d2ee
Carl Pearson <cap10@ufl.edu>
2015-03-02 18:07:23 -0500

    more user location analysis / plotting

--- rscripts/data_review.R
+++ rscripts/data_review.R
@@ -1,22 +1,32 @@
 ## analysis and plots of montreal data
 
-require(data.table); require(ggplot2)
+require(data.table); require(ggplot2); require(grid)
 
-src.dt <- data.table(read.csv("~/Dropbox/montreal/merged.o", header = F))
+src.dt <- data.table(read.csv("~/Dropbox/montreal/merged.o", header = F, sep=" "))
 setnames(src.dt, c("user_id","location_id","login","logout"))
 
 max_real_duration <- 60*60*24 # at most, continuous login of a day
 
 duration.censored.dt <- src.dt[(logout - login) <= max_real_duration,]
 
-user.location.count.dt <- duration.censored.dt[,list(location_count = length(unique(location_id))), by="user_id"]
+user.location.count.dt <- duration.censored.dt[,
+	list(location_count = length(unique(location_id))),
+	by="user_id"
+]
 valid.users <- user.location.count.dt[location_count > 1,user_id]
 
 user_and_duration.censored.dt <- duration.censored.dt[user_id %in% valid.users,]
 
-locs.dt <- user_and_duration.censored.dt[, list(unique_users = length(unique(user_id)), total_login_time = sum(logout - login)), by="location_id"] # unique users per location
+locs.dt <- user_and_duration.censored.dt[,
+	list(
+		unique_users = length(unique(user_id)),
+		total_login_time = sum(logout - login)),
+	by="location_id"
+] # unique users per location
 
-invalid.locs <- locs.dt[(unique_users == 1) | (total_login_time < 60*60), location_id] # locations with only one user, or total log in time < 1 hour
+min_total_login <- 60*60 # 1 hour
+
+invalid.locs <- locs.dt[(unique_users == 1) | (total_login_time < min_total_login), location_id] # locations with only one user, or total log in time < 1 hour
 
 loc_user_duration.censored.dt <- user_and_duration.censored.dt[!(location_id %in% invalid.locs),]
 
@@ -28,9 +38,41 @@ loc_view.dt <- loc_user_duration.censored.dt[,
     sd_login = sd(logout - login)
   ),
   by="location_id"]
+
+user_view.dt <- loc_user_duration.censored.dt[,
+	list(
+		unique_locs = length(unique(location_id)),
+		total_login_time = sum(logout - login),
+		mean_login = mean(logout - login),
+		sd_login = sd(logout - login)
+	),
+	by="user_id"
+]
+
+loc_count_breaks <- hist(user_view.dt$unique_locs, plot = F)$breaks
+
+loc_count_cats <- paste(head(loc_count_breaks, -1), tail(loc_count_breaks, -1), sep="-")
+
+user_view.dt[,loc_count_cat := sapply(unique_locs, function(n) which.max(n <= loc_count_breaks)-1)]
+user_view.dt$loc_count_cat <- factor(loc_count_cats[user_view.dt$loc_count_cat], levels=loc_count_cats)
+
 # sum(loc_user_duration.censored.dt[,length(unique(location_id)),by="user_id"]$V1 == 1) == 0 # => no more user ids to censor
 
-p <- ggplot(loc_user_duration.censored.dt) + theme_bw()
+total_login_duration <- sum(as.numeric(loc_view.dt$total_login_time))
+loc_view.dt[,proportion := total_login_time/total_login_duration]
+
+sample(loc_view.dt$location_id, 5, F, loc_view.dt$proportion)
+
+p.loc <- ggplot(loc_view.dt) + theme_bw()
+p.user <- ggplot(user_view.dt) + theme_bw()
+
+p.user + theme(panel.margin = unit(0.5, "lines")) +
+	aes(fill=loc_count_cat, x=total_login_time/60) +
+	facet_grid(loc_count_cat ~ ., scales="free_y", shrink=T, drop=F) +
+	geom_bar() + scale_x_log10(name="total login time per individual, in minutes") +
+	ylab("number of individuals") +
+	scale_fill_discrete(name="locations per individual", drop=F) +
+	scale_y_continuous(breaks=function(lims) c(head(lims,1), tail(lims, 1)), expand=c(0,0))
 
 p + aes(x=(logout - login)) + geom_bar() + geom_vline(x=150, color="red") + scale_x_log10()
 
