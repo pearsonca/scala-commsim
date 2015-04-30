84103aa2db3ade569ac154c70d1b19475dbe78bb
Carl Pearson <cap10@ufl.edu>
2015-04-30 06:59:52 -0400

    revisions to haunt gen

--- src/main/scala/edu/cap10/actormodels/demo/HauntGenerators.scala
+++ src/main/scala/edu/cap10/actormodels/demo/HauntGenerators.scala
@@ -1,10 +1,12 @@
 package edu.cap10.actormodels.demo
 
 import scala.util.Random
+import edu.cap10.util.NaturalInt
+import scala.languageFeature.implicitConversions
 
 case class HauntGenerator(rng : Random) {
 
-  def uniform(locations : Seq[Int]) : (Int) => Seq[Int] = {
+  def uniform(locations : Seq[LocationID]) : (Int) => Seq[LocationID] = {
     require(locations.size > 0, "Cannot sample from non-existent locations.")
     (count : Int) => {
       require(count >= 0, "Cannot request a negative location count.")
@@ -13,6 +15,15 @@ case class HauntGenerator(rng : Random) {
     } ensuring(_.size == count)
   }
  
-  def distributed(locations: Seq[Int])(mean:Double) : Seq[Int] = ???
+  def distributed(locations: Seq[LocationID], min : Int = 1) : (Double) => Seq[LocationID] = {
+    require(locations.size > 0, "Cannot sample from non-existent locations.")
+    (mean : Double) => {
+      require(mean >= min, "Cannot request a mean number of locations less than the min "+min)
+      require(mean < locations.size, "The expected number of locations can be the total number of locations.")
+      val p = (mean - min) / (locations.size - min)
+      val (keep, possible) = rng.shuffle(locations).splitAt(min)
+      keep ++ possible.filter { _ => rng.nextDouble < p }
+    } ensuring(_.size >= min)
+  }
   
 }
