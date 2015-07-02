fedb6510915e1362235139306112d8e3b5a31470
Carl Pearson <cap10@ufl.edu>
2015-07-02 11:30:00 -0400

    update infection model

--- src/main/scala/edu/cap10/actormodels/tb/InfectionModel.scala
+++ src/main/scala/edu/cap10/actormodels/tb/InfectionModel.scala
@@ -1,17 +1,19 @@
 package edu.cap10.actormodels.tb
 
-import TBStrain.Value
+import TBStrain.{Value => Strain}
 import edu.cap10.util.LocalRNG
 import edu.cap10.util.Probability
+import edu.cap10.util.Probability._
+import scala.language.implicitConversions
 
 case class InfectionModel(mp:ModelParams, val seed:Long)
-  extends Function2[HostTBState, Seq[TBStrain.Value], Option[HostTBState]]
+  extends Function2[HostTBState, Seq[Strain], Option[HostTBState]]
   with LocalRNG
 {
   import mp._
   def apply(
     hostTBstate : HostTBState,
-    infectiousContacts:Seq[TBStrain.Value]
+    infectiousContacts:Seq[Strain]
   ) : Option[HostTBState] = if (infectiousContacts.isEmpty)
     None
   else
@@ -23,7 +25,7 @@ case class InfectionModel(mp:ModelParams, val seed:Long)
       case Resistant(_) | Exposed(_) | Infectious(_) | Treated(_) => None
     }
   
-  def anyInfection(contacts:Seq[TBStrain.Value], success:Probability) : Option[TBStrain.Value] = 
+  def anyInfection(contacts:Seq[TBStrain.Value], success:Probability) : Option[Strain] = 
     contacts.find(_ => rng.nextDouble() < success)
   
 }
