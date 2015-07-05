fbf450a3037196cdf4504d329574caf59b987fc2
Carl Pearson <cap10@ufl.edu>
2015-07-05 08:14:22 -0400

    add login, logout capability

--- src/main/scala/edu/cap10/actormodels/covert/HotSpot.scala
+++ src/main/scala/edu/cap10/actormodels/covert/HotSpot.scala
@@ -1,5 +1,7 @@
 package edu.cap10.actormodels.covert
 
+import scala.collection.mutable.{Map => MMap}
+
 sealed trait HotSpotState
 case object NotYetActive extends HotSpotState
 case object Active extends HotSpotState
@@ -8,21 +10,40 @@ case object NoLongerActive extends HotSpotState
 case class HotSpot(id:Long, logName:String, activationDate:Long, shutdownDate:Long) {
   
   private var _clock : Long = 0
-  private var _state = if (activationDate == 0) Active else NotYetActive
+  private var _state : HotSpotState = if (activationDate == 0) Active else NotYetActive
   
   def clock : Long = _clock
   
   def tick(time:Long) : HotSpotState = {
     _clock += 1
-    if (_clock == activationDate)
-      _state = Active
-    else if (_clock == shutdownDate)
-      _state = NoLongerActive
+    if (_clock == activationDate)    _setState(Active)
+    else if (_clock == shutdownDate) _setState(NoLongerActive)
     _state 
   }
-  // receive ticks
-    // once tick == activationDate, activate
-    // once tick == shutdownDate, deactive
+
+  private def _record(userID:Long, timeIn:Long, timeOut:Long) : Boolean = {
+    true
+  }
+  
+  private def _setState(s : HotSpotState) = {
+    _state = s
+  }
+  
+  private val _cache : MMap[Long, Long] = MMap() 
+  
+  def login(userID:Long, time:Long) = if (_state == Active) {
+    if (_cache contains userID)
+      false
+    else
+      _cache.put(userID, time).isEmpty
+  } else {
+    false
+  }
+  
+  def logout(userID:Long, time:Long) = _cache remove userID match {
+    case Some(loginTime) => true
+    case None => false
+  }
   
   // receive logins, logouts
     // reject if not active
--- src/test/scala/edu/cap10/actormodels/covert/HotSpotTests.scala
+++ src/test/scala/edu/cap10/actormodels/covert/HotSpotTests.scala
@@ -32,4 +32,37 @@ class HotSpotTests extends FunSuite with BeforeAndAfter {
     assert(HS1.tick(turnoff) !== Active)
   }
   
+  test("HotSpot tick returns NoLongerActive after it turnsoff") {
+    val (turnon, turnoff) = (10, 20)
+    val HS1 = HotSpot(1, "log", turnon, turnoff)
+    for (i <- 1 until turnoff) HS1.tick(i)
+    assert(HS1.tick(turnoff) === NoLongerActive)
+    assert(HS1.tick(turnoff+1) === NoLongerActive)
+  }
+  
+  test("when not active, logging in returns false") {
+    val HS1 = HotSpot(1, "log", 10, 10)
+    assert(HS1.login(1, 0) === false)
+    assert(HS1.login(2, 0) === false)
+  }
+  
+  test("when active, logging in with a new user returns true") {
+    val HS1 = HotSpot(1, "log", 0, 10)
+    assert(HS1.login(1, 0) === true)
+    assert(HS1.login(2, 0) === true)
+  }
+  
+  test("when active, logging in with a user that hasn't logged out returns false") {
+    val HS1 = HotSpot(1, "log", 0, 10)
+    assert(HS1.login(1, 0) === true)
+    assert(HS1.login(1, 0) === false)
+  }
+  
+  test("when active, logging out with a user resets their ability to login") {
+    val HS1 = HotSpot(1, "log", 0, 10)
+    assert(HS1.login(1, 0) === true)
+    HS1.logout(1, 1)
+    assert(HS1.login(1, 2) === true)    
+  }
+  
 }
