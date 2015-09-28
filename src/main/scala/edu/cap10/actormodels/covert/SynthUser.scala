4bfb401d7b2723572609593588303e779b63d2bd
Carl Pearson <cap10@ufl.edu>
2015-09-28 10:13:17 -0400

    synth user tweak

--- src/main/scala/edu/cap10/actormodels/covert/SynthUser.scala
+++ src/main/scala/edu/cap10/actormodels/covert/SynthUser.scala
@@ -9,10 +9,28 @@ case class LoginEvent(user_id:UserID, location_id:HotSpotID, login:Long, logout:
  */
 case class SynthUser(id:Int, shape:Double, mean:Double, 
     locations:Array[Location], prefCDF:Array[Double]) {
+  // draw next event day
+  // on event day, draw number of events
+  // if meeting day, replace events with meeting
+  
   val gen = Gamma(shape, mean/shape)
   val searchSrc = scala.collection.Searching.search(prefCDF)
   val rng = new scala.util.Random(id)
-  var day = 0
+
+  var tilEvent : Int = -1
+  var meeting : Option[Location] = None
+  def meetAt(l:Location) = { meeting = Some(l) }
+  
+  def tick = {
+    if (tilEvent == -1) {
+      // draw new time tilEvent
+    } else {
+      if (tilEvent == 0 || !meeting.isEmpty) {
+        
+      }
+    }
+  }
+  
   def draw = {
     val t = gen.draw()
     val day = (t/(24*60*60)).round // days til next event
