9e753d0d6bfcf7211ce9768d164eb8bac53568ae
Carl Pearson <cap10@ufl.edu>
2016-02-11 10:44:23 -0500

    closer on properly assembling inputs

--- analyze.R
+++ analyze.R
@@ -8,6 +8,8 @@ require(parallel)
 args <- commandArgs(trailingOnly = T)
 args <- c("output/process-test", "output/analyze-test", "input")
 
+locLimits <- readRDS("input/location-lifetimes.rds")
+
 getSimRes <- function(tar="cc") list.files(args[1], pattern = paste0(tar,".csv"), full.names = T)
 
 # cnt <- args[1]
@@ -21,20 +23,23 @@ src.dt <- readRDS(paste0(args[3],"/raw-input.rds"))
 
 #src.dt <- src.dt[delta != 0,]
 max.uid <- src.dt[,max(user_id)]+1
-min.login <- src.dt[,min(login)]
-oldorig <- trunc(as.POSIXlt("2005-01-01", tz="EST"), "days")
-neworig <- trunc(as.POSIXlt(min.login, origin = "2005-01-01", tz = "EST"), "days")
-del <- as.numeric(neworig)-as.numeric(as.POSIXlt("2005-01-01"))
+#min.login <- src.dt[,min(login)]
+#oldorig <- trunc(as.POSIXlt("2005-01-01", tz="EST"), "days")
+#neworig <- trunc(as.POSIXlt(min.login, origin = "2005-01-01", tz = "EST"), "days")
+#del <- as.numeric(neworig)-as.numeric(as.POSIXlt("2005-01-01"))
 
 readELtable <- function(tar, offsetid) {
-  res <- data.table(fread(tar,
-                          header = F, col.names = c("user.a", "user.b", "location_id", "login", "logout", "type")),
-                    key = c("login", "logout", "location_id", "user.a", "user.b")
+  res <- data.table(fread(
+    tar, header = F,
+    col.names = c("user.a", "user.b", "location_id", "login", "logout", "type")
+  ), key = c("location_id", "login", "logout", "user.a", "user.b")
   )
+  # trim locations ids
+  
   res[user.a < 0, user.a := -user.a + offsetid]
   res[user.b < 0, user.b := -user.b + offsetid]
   res[user.b < user.a, `:=`(user.b = user.a, user.a = user.b)]
-  res[,list(user.a, user.b),keyby=list(login=login-del,logout=logout-del)]
+  res[,list(user.a, user.b, location_id), keyby=list(login, logout)]
 }
 
 base.dt <- readRDS(paste0(args[3],"/raw-pairs.rds"))
@@ -43,7 +48,11 @@ base.dt[
   `:=`(user.b = user.a, user.a = user.b)
 ]
 
-base.dt <- setcolorder(base.dt[,list(user.a=user.a+1, user.b=user.b+1),keyby=list(login=start-del,logout=end-del)], c("user.a","user.b","login","logout"))
+base.dt[,
+  `:=`(login = start, logout = end)
+]
+
+base.dt <- setcolorder(base.dt[,list(user.a=user.a+1, user.b=user.b+1),keyby=list(login,logout)], c("user.a","user.b","login","logout"))
 
 slice <- function(el.dt, start.day, end.day) {
   res <- el.dt[(logout < end.day*24*3600) & (login > start.day*24*3600), .N, keyby=list(user.a, user.b)]
@@ -53,23 +62,26 @@ slice <- function(el.dt, start.day, end.day) {
 }
 
 resolve <- function(cc.pairs, cu.pairs, increment=30, st=365) mapply(function(cc.file, cu.file) {
-  cc.EL <- readELtable(cc.file, max.uid)[,list(user.a,user.b,login,logout)]
-  cu.EL <- readELtable(cu.file, max.uid)[,list(user.a,user.b,login,logout)]
+  cc.EL <- readELtable(cc.file, max.uid)
+  cu.EL <- readELtable(cu.file, max.uid)
   count <- cc.EL[,length(unique(c(user.a,user.b)))]
   covert.ids <- max.uid + 1:count
-  combo.EL <- rbindlist(list(base.dt, cc.EL, cu.EL))
-  days <- seq(st,5*365,by=increment)
-  res <- mclapply(days, function(day) {
+  covs <- locLimits[setkey(rbind(cc.EL, cu.EL), location_id)][logout > arrive & login < depart][,list(user.a,user.b,login,logout)]
+  combo.EL <- rbind(base.dt, covs)
+  myears <- round(max(combo.EL$logout)/60/60/24/365)
+  days <- seq(st,myears*365,by=increment)
+  res <- lapply(days, function(day) {
     sl = slice(combo.EL, day-increment, day)
     gg <- graph(t(sl$res), directed=F)
     cs <- fastgreedy.community(gg)
+    #browser()
     members <- membership(cs)[sl$mp[user_id %in% covert.ids]$new_user_id]
     comm.counts <- sizes(cs)[members]
     cat(day, "\n")
     list(m=members,co=comm.counts,d=day)
   })
-  members.wh <- file(gsub("cc\\.csv", "members.csv", cc.file), open = "a")
-  sizes.wh <- file(gsub("cc\\.csv", "sizes.csv", cc.file), open = "a")
+  members.wh <- file(gsub("cc\\.csv", "members.csv", cc.file), open = "w")
+  sizes.wh <- file(gsub("cc\\.csv", "sizes.csv", cc.file), open = "w")
   lapply(res, function(l) with(l,{
     write(c(d,m), members.wh, ncolumns = count+1, append = T)
     write(c(d,co), sizes.wh, ncolumns = count+1, append = T)
